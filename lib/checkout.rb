b5cf562757300c3031c9f5cb2f2cb42a47af27af
Robin Borjesson <robin.b@outlook.com>
2018-08-30 17:00:53 +0200

    Add scan method to checkout class

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -1,10 +1,15 @@
 class Checkout
 
-    attr_accessor :promotional_rules, :discount_threshold, :discount
+    attr_accessor :promotional_rules, :discount_threshold, :discount, :product_discount
 
     def initialize(promotional_rules = {})
         @promotional_rules = promotional_rules
-        load_promotional_rules unless promotional_rules.empty?        
+        load_promotional_rules unless promotional_rules.empty?
+        @shopping_cart = {}        
+    end
+
+    def scan(item)
+        @shopping_cart.keys.include?(:item) == nil ? @shoppin_cart[:item] = 1 : @shoppint_cart[:item] +=1
     end
 
     
@@ -14,14 +19,9 @@ class Checkout
     def load_promotional_rules
         @discount_threshold = @promotional_rules[:discount_threshold]
         @discount = @promotional_rules[:discount_value]
-
-    end
-
-    def extract_general_discount
+        @product_discount = @promotional_rules[:product_discount]
     end
     
-    def extract_product_discount
-    end
 
 
 end
--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -5,9 +5,9 @@ describe Checkout do
     promotional_rules = {discount_threshold: 60, 
         discount_value: 0.1,
         product_discount: {
-            product_code: 001,
-            number_threshold: 2,
-            discount_price: 8.5 }}
+            '001': { 
+                number_threshold: 2,
+                discount_price: 8.5 }}}
 
     subject { described_class.new(promotional_rules) }
 
@@ -18,8 +18,10 @@ describe Checkout do
     it 'should be able to access discount values' do 
         expect(subject.discount_threshold).to eq 60
         expect(subject.discount).to eq 0.1
+        expect(subject.product_discount).to_not be nil
     end
 
-
-
+    it 'should be able to scan products' do 
+        expect(subject.scan('001')).to be_truthy
+    end
 end
