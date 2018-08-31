87bab8a6fca3d6a7377a0101204480b8492fefcb
Robin Borjesson <robin.b@outlook.com>
2018-08-31 14:17:57 +0200

    Add ability to calculate price with promo rules

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -1,20 +1,52 @@
 class Checkout
 
-    attr_accessor :promotional_rules, :discount_threshold, :discount, :product_discount, :shopping_cart
+    attr_accessor :promotional_rules, :discount_threshold, :discount, 
+                    :product_discount, :shopping_cart, :inventory
 
     def initialize(promotional_rules = {})
         @promotional_rules = promotional_rules
         load_promotional_rules unless promotional_rules.empty?
-        @shopping_cart = {}        
+        @shopping_cart = {}
+        load_inventory        
     end
 
     def scan(item)
-        @shopping_cart[item.to_sym] == nil ? @shopping_cart[item.to_sym] = 1 : @shopping_cart[item.to_sym] += 1
+        @shopping_cart[item.to_sym].nil? ? @shopping_cart[item.to_sym] = 1 : @shopping_cart[item.to_sym] += 1
     end
 
-    
+    def total
+        calculate_price
+    end
 
     private
+
+    def calculate_price
+        total_sum = 0        
+        @shopping_cart.each do |product_code, number_of_items|
+            sub_total = 0
+            sub_total = get_price(product_code, number_of_items) * number_of_items
+            total_sum += sub_total       
+        end
+        total_sum > @discount_threshold ? (total_sum * (1 - @discount)).round(2) : total_sum.round(2)           
+    end
+
+    def get_price(product_code, number_of_items)
+        
+        if @product_discount[product_code.to_sym] != nil
+            calculate_product_discount(product_code, number_of_items)
+        else 
+            @inventory[product_code.to_sym][:price]            
+        end
+              
+    end
+
+    def calculate_product_discount(product_code, number_of_items)
+        if number_of_items >= @product_discount[product_code.to_sym][:number_threshold]
+            @product_discount[product_code.to_sym][:discount_price]
+        else
+            @inventory[product_code.to_sym][:price]
+        end        
+    end
 
     def load_promotional_rules
         @discount_threshold = @promotional_rules[:discount_threshold]
@@ -22,6 +54,18 @@ class Checkout
         @product_discount = @promotional_rules[:product_discount]
     end
     
+    def load_inventory
+        @inventory = {
+                    '001': {
+                        name: "Lavender heart",
+                        price: 9.25},
+                    '002': {
+                        name: "Personalised cufflinks",
+                        price: 45},
+                    '003': {
+                        name: "Kids T-shirt",
+                        price: 19.95} }
+    end
 
 
 end
--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -8,6 +8,15 @@ describe Checkout do
             '001': { 
                 number_threshold: 2,
                 discount_price: 8.5 }}}
+    inventory = {'001': {
+                    name: "Lavender heart",
+                    price: 9.25},
+                '002': {
+                    name: "Personalised cufflinks",
+                    price: 45},
+                '003': {
+                    name: "Kids T-shirt",
+                    price: 19.95} }
 
     subject { described_class.new(promotional_rules) }
 
@@ -31,4 +40,37 @@ describe Checkout do
         expect(subject.shopping_cart[:"001"]).to eq 2
     end
 
+    it 'should be able to show which items have currently been scanned' do
+        subject.scan('001')
+        subject.scan('001')
+        subject.scan('002')
+        expect(subject.shopping_cart).to eq({'001': 2, '002': 1})
+    end       
+
+    it 'should be able to provide items and prices in store' do
+        expect(subject.inventory).to eq inventory
+    end
+
+    it 'should be able to provide the total price of scanned items' do
+        subject.scan('001')
+        subject.scan('003')              
+        expect(subject.total). to eq 29.2
+    end
+
+    it 'should be able to incorporate global discount according to promotional rules' do
+        subject.scan('001')
+        subject.scan('002')
+        subject.scan('003')
+        expect(subject.total).to eq 66.78   
+    end
+
+    it 'should be able to incorporate product discount based on number of items' do
+        subject.scan('001')
+        subject.scan('002')
+        subject.scan('003')
+        subject.scan('001')
+        expect(subject.total).to eq 73.76
+        
+    end
+    
 end
